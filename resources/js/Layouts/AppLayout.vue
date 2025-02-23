e0bf0b14ae44dc2d727c90133361f7d48816dba1
Kenn <johnkenneth.ornias@gmail.com>
2025-02-24 07:08:25 +0800

    fix: navlink error for sending notifications

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue';
 import { Head, Link, router } from '@inertiajs/vue3';
 import Banner from '@/Components/Banner.vue';
-import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 
@@ -46,9 +45,8 @@ const notify = () => {
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink @click="notify">
-                                📧 Send Notifications
-                            </NavLink>
+                            <div @click="notify" class="ms-1 cursor-pointer text-sm">📧 Send Notifications
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -59,12 +57,6 @@ const notify = () => {
                     </button>
                     <div v-if="showAccountDropdown" class="absolute bottom-full left-0 w-full mb-2 bg-white dark:bg-gray-700 rounded-md shadow-lg z-50">
                         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200">
-                            <li>
-                                <DropdownLink :href="route('profile.show')">Profile</DropdownLink>
-                            </li>
-                            <li v-if="$page.props.jetstream.hasApiFeatures">
-                                <DropdownLink :href="route('api-tokens.index')">API Tokens</DropdownLink>
-                            </li>
                             <li>
                                 <form @submit.prevent="logout">
                                     <DropdownLink as="button">Log Out</DropdownLink>
@@ -78,33 +70,6 @@ const notify = () => {
 
         <div class="flex-1 sm:ml-64">
             <main class="min-h-screen">
-                <!-- <header v-if="$slots.header" class="bg-indigo-950 dark:bg-gray-900 flex items-center justify-between px-6 py-4">
-                    <slot name="header" />
-                    <Dropdown align="right" width="48">
-                        <template #trigger>
-                            <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url" :alt="$page.props.auth.user.name">
-                            </button>
-                            <span v-else class="inline-flex rounded-md">
-                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                    {{ $page.props.auth.user.name }}
-                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                    </svg>
-                                </button>
-                            </span>
-                        </template>
-                        <template #content>
-                            <div class="block px-4 py-2 text-xs text-gray-400">Manage Account</div>
-                            <DropdownLink :href="route('profile.show')">Profile</DropdownLink>
-                            <DropdownLink v-if="$page.props.jetstream.hasApiFeatures" :href="route('api-tokens.index')">API Tokens</DropdownLink>
-                            <div class="border-t border-gray-200" />
-                            <form @submit.prevent="logout">
-                                <DropdownLink as="button">Log Out</DropdownLink>
-                            </form>
-                        </template>
-                    </Dropdown>
-                </header> -->
                 <slot />
             </main>
         </div>
