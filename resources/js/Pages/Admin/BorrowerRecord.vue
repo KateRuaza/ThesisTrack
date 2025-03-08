7562cb6786800c7d076654a44aa22d16a2c80d98
Kenn <johnkenneth.ornias@gmail.com>
2025-03-08 08:38:06 +0800

    feat: admin can search by name and filter by month

--- resources/js/Pages/Admin/BorrowerRecord.vue
+++ resources/js/Pages/Admin/BorrowerRecord.vue
@@ -1,15 +1,26 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { watch, computed, ref } from 'vue';
 import { router, Link } from '@inertiajs/vue3';
 import { formatDate } from '@/helper.js';
+import { debounce } from 'lodash';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import BorrowerStatus from '@/Components/BorrowerStatus.vue';
 import Pagination from '@/Components/Pagination.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
 
 const props = defineProps({
     borrowers: Object,
 });
 
+const filterName = ref('');
+const filterStartMonth = ref('');
+const filterEndMonth = ref('');
+
+const showPopup = ref(false);
+const showDateRangeFilter = ref(false);
 const hasPagination = computed(() => props.borrowers.links && props.borrowers.links.length > 3);
 
 const updateStatus = (borrower, status) => {
@@ -25,11 +36,35 @@ const updateStatus = (borrower, status) => {
     });
 };
 
-const showPopup = ref(null);
-
-const togglePopup = (id) => {
+const toggleStatusPopUp = (id) => {
     showPopup.value = showPopup.value === id ? null : id;
 };
+
+const toggleDateRangeFilter = () => {
+    showDateRangeFilter.value = !showDateRangeFilter.value;
+};
+
+const clearFilters = () => {
+    filterName.value = '';
+    filterStartMonth.value = '';
+    filterEndMonth.value = '';
+    showDateRangeFilter.value = false;
+
+    router.get(route('borrowers.index'), {
+        filterName: '',
+        filterStartMonth: '',
+        filterEndMonth: ''
+    }, { preserveState: true });
+};
+
+watch([filterName, filterStartMonth, filterEndMonth], debounce(() => {
+    router.get(route('borrowers.index'), {
+        filterName: filterName.value,
+        filterStartMonth: filterStartMonth.value,
+        filterEndMonth: filterEndMonth.value
+    }, { preserveState: true });
+}, 500));
+
 </script>
 
 <template>
@@ -41,7 +76,14 @@ const togglePopup = (id) => {
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-4">
+                <div class="flex items-center justify-between gap-x-2">
+                    <TextInput v-model="filterName" placeholder="Search.." class="me-4"/>
+                    <div class="space-x-2">
+                        <SecondaryButton @click="toggleDateRangeFilter">Filter</SecondaryButton>
+                        <SecondaryButton>Generate Report</SecondaryButton>
+                    </div>
+                </div>
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                     <div class="p-6 overflow-x-auto">
                         <div class="w-full overflow-x-auto">
@@ -59,24 +101,13 @@ const togglePopup = (id) => {
                                 </thead>
                                 <tbody>
                                     <tr v-for="borrower in borrowers.data" :key="borrower.id" class="bg-white dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300">
-                                            <Link :href="route('borrowers.show', borrower.id)">{{ borrower.name }}</Link>
-                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300"><Link :href="route('borrowers.show', borrower.id)">{{ borrower.name }}</Link></td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ borrower.thesis_name }}</td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ formatDate(borrower.created_at) }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">
-                                            {{ borrower.returned_at ? formatDate(borrower.returned_at) : '' }}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">
-                                            {{ borrower.first_warning ? "Sent" : "" }}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">
-                                            {{ borrower.last_warning ? "Sent" : "" }}
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm">
-                                            <BorrowerStatus :borrower="borrower" :showPopup="showPopup" @togglePopup="togglePopup" @updateStatus="updateStatus" />
-                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ borrower.returned_at ? formatDate(borrower.returned_at) : '' }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ borrower.first_warning ? "Sent" : "" }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ borrower.last_warning ? "Sent" : "" }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm"><BorrowerStatus :borrower="borrower" :showPopup="showPopup" @toggleStatusPopUp="toggleStatusPopUp" @updateStatus="updateStatus" /></td>
                                     </tr>
                                     <tr v-if="borrowers.data.length === 0">
                                         <td colspan="4" class="px-6 py-4 whitespace-nowrap text-center text-sm text-gray-500 dark:text-gray-400">No borrowers found.</td>
@@ -87,6 +118,22 @@ const togglePopup = (id) => {
                         <div v-if="hasPagination" class="mt-4">
                             <Pagination :links="borrowers.links" />
                         </div>
+                    </div>
+                </div>
+                <div v-if="showDateRangeFilter" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-96 space-y-4">
+                        <div class="grid grid-cols-2 gap-2 ">
+                            <div class="">
+                                <InputLabel>Start</InputLabel>
+                                <TextInput v-model="filterStartMonth" type="date"/>
+                            </div>
+                            <div class="">
+                                <InputLabel>End</InputLabel>
+                                <TextInput v-model="filterEndMonth" type="date"/>
+                            </div>
+                        </div>
+                        <PrimaryButton @click="toggleDateRangeFilter" class="w-full justify-center">Close</PrimaryButton>
+                        <SecondaryButton @click="clearFilters" class="w-full justify-center">Reset</SecondaryButton>
                     </div>
                 </div>
             </div>
