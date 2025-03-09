24ade1e0b63f50cd5edf90be822812c6170569f5
Kenn <johnkenneth.ornias@gmail.com>
2025-03-09 15:33:52 +0800

    feat: insert default admin account

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -37,6 +38,12 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
+        DB::table('users')->insert([
+            'name' => fake()->name(),
+            'email' => 'admin@email.com',
+            'password' => bcrypt('password'),
+        ]);
     }
 
     /**
